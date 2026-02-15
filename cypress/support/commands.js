a2b04fe70fa546066ae43df6c78730cd6d9ff33e
Alice <alice.bastos@bridge.ufsc.br>
2026-02-14 21:05:50 -0300

    lesson 2 - exercício 7.2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,17 @@
-Cypress.Commands.add("fillMandatoryFieldsAndSubmit", () => {
-  cy.get('input[name="firstName"]').type("Alice");
-  cy.get('input[name="lastName"]').type("Motin");
-  cy.get('input[id="email"]').type("alice@gmail.com");
-  cy.get('textarea[name="open-text-area"]').type("Curso muito bom, gostei!");
-  cy.get('button[type="submit"]').click();
-});
+Cypress.Commands.add(
+  "fillMandatoryFieldsAndSubmit",
+  (
+    data = {
+      firstName: "Alice",
+      lastName: "Motin",
+      email: "alice@gmail.com",
+      text: "Curso muito bom, gostei!",
+    }
+  ) => {
+    cy.get('input[name="firstName"]').type(data.firstName);
+    cy.get('input[name="lastName"]').type(data.lastName);
+    cy.get('input[id="email"]').type(data.email);
+    cy.get('textarea[name="open-text-area"]').type(data.text);
+    cy.get('button[type="submit"]').click();
+  }
+);
